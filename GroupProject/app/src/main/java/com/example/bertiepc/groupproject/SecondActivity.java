1d2b44b5fd64488b48dbcdeb746bfcb145f97303
Albertus P Coetzee <coetzeealbertus@gmail.com>
2017-05-18 00:32:09 +0100

    update

--- GroupProject/app/src/main/java/com/example/bertiepc/groupproject/SecondActivity.java
+++ GroupProject/app/src/main/java/com/example/bertiepc/groupproject/SecondActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -38,13 +39,13 @@ public class SecondActivity extends AppCompatActivity {
 
     public void displayPlaces(String selectedTown, String selectedType){
 
-        ArrayList<String> theList = new ArrayList<>();
+        ArrayList<String> placeNameList = new ArrayList<>();
+
         Cursor data = db.getPlaces(selectedTown,selectedType);
 
         while (data.moveToNext()){
-            theList.add(data.getString(1));
-            ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, theList);
-            places.setAdapter(listAdapter);
+            placeNameList.add("\n \n" + data.getString(1) + "\n \n" + data.getString(8) + "\n");
+            ListAdapter nameListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, placeNameList);places.setAdapter(nameListAdapter);
         }
 
     }
